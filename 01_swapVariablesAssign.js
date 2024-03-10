773bea8ea5b0189d4349b4a1f41603fb53078998
Amrutasonawane30 <160743814+Amrutasonawane30@users.noreply.github.com>
2024-03-11 00:35:53 +0530

    Added Assignment 1.2 swap variable
    atomatic upload stopped

---
+++ 01_swapVariablesAssign.js
@@ -0,0 +1,21 @@
+console.log("Swap two variables using third variable");
+console.log("===========Before Swap==========");
+var sweety="Sweety", cutie="Cutie", temp;
+console.log("Sweety = ",sweety, "and Cutie = ",cutie);
+
+console.log("===========After Swap==========");
+sweety=cutie; cutie=temp; temp=sweety;
+console.log("Sweety = ",sweety, "and Cutie = ",cutie);
+
+console.log("Swap three variables using third variable");
+console.log("===========Before Swap==========");
+var num1=100, num2=200, num3=300,temp;
+console.log("num1:",num1,"num2:",num2, "num3:",num3);
+console.log("===========After Swap==========");
+
+temp=num1;
+num1=num2;
+num2=num3;
+num3=temp; 
+console.log("num1:",num1,"num2:",num2, "num3:",temp);
+
