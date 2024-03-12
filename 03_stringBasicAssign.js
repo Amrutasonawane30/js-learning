cf0fffcbffc35d097ec497ee492e258a3cedf6f9
Amrutasonawane30 <amruta.sonawane.rga@gmail.com>
2024-03-13 00:23:02 +0530

    string basics assignment
    string basics assignment done

---
+++ 03_stringBasicAssign.js
@@ -0,0 +1,12 @@
+
+function stringBasic(){
+    var hob1="traveling",hob2="cooking",hob3="singing";
+    console.log("____________Step 1_____________");
+    console.log(`My Dream company is "Amdocs."`);
+    console.log("____________Step 2_____________");
+    console.log(`My hobbies are ${hob1}, ${hob2} and ${hob3}.`);
+    console.log("____________Step 3_____________");
+    var tot=hob1.length+hob2.length+hob3.length;
+    console.log(`The length of ${hob1}, ${hob2} and ${hob3} is `,tot);
+}
+stringBasic();
---
+++ 04_stringTemplates.js
@@ -0,0 +1,5 @@
+
+console.log(`I am "software" developer`);
+
+var city = "Pune",firstName="Amruta";
+console.log(`My name is ${firstName} and city is ${city}`);
