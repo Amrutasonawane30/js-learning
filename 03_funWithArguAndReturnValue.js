1126d3be7dd70308c78f594df93b3ce7256aaad0
Amrutasonawane30 <160743814+Amrutasonawane30@users.noreply.github.com>
2024-03-12 22:32:37 +0530

    Add files via upload

---
+++ 03_funWithArguAndReturnValue.js
@@ -0,0 +1,11 @@
+function subtract(n1,n2){
+console.log("===========Subtraction===========");
+console.log("Arguments ",n1,n2);
+var result=n1-n2;
+return result;
+}
+
+var returnValue = subtract(9, 4);
+console.log("Result of subtraction is: ", returnValue);
+var returnValue = subtract(99, 42);
+console.log("Result of subtraction is: ", returnValue);
---
+++ 03_functionNoArguNOreturn.js
@@ -0,0 +1,21 @@
+//1. Function with arguments and no return value
+
+function add(n1, n2, n3){
+
+    console.log("Arguments: ", n1, n2, n3);
+
+    var result = n1+n2+n3;
+
+    console.log("Addition is: ", result);
+
+}
+
+add(3, 6, 7);
+
+add(99.89, 67, 456789);
+
+add(6, 7);
+
+add(16, 9, 90, 44);
+
+add("Hi", "Good", "Morning")
