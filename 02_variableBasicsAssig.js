6d27c241d86730579aee6d984c82267b606aab4c
Amrutasonawane30 <amruta.sonawane.rga@gmail.com>
2024-03-15 22:45:54 +0530

    did assignment 2 of javascript using different datat type
    difference between null and undefined

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,9 @@
+console.log("________________Step 1________________");
+var var1="Good Morning", var2=true,var3=432516;
+console.log("Data type for variable1 is ",typeof var1);
+console.log("Data type for variable2 is ",typeof var2);
+console.log("Data type for variable3 is ",typeof var3);
+
+console.log("________________Step 2________________");
+console.log("Difference Between null and undefined");
+console.log("Null is an assigned value. It means nothing. Undefined means a variable has been declared but not defined yet. null is an object.");
